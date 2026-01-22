c3366d96dbd06a94336740a74956fb1105ea039b
Barney Rye <barneyrye@btinternet.com>
2026-01-22 21:48:36 +0000

    Started main code and most of ILI9341 driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "adc.h"
+#include "dma.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
@@ -38,7 +39,42 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define PAUSE_PIN GPIOC, GPIO_PIN_13
+#define CHL_SELECT_PIN GPIOC, GPIO_PIN_14
+#define HSE_IN_PIN GPIOH, GPIO_PIN_0
+#define HSE_OUT_PIN GPIOH, GPIO_PIN_1
+#define TIM5_CH1_PIN GPIOA, GPIO_PIN_0
+#define TIM5_CH2_PIN GPIOA, GPIO_PIN_1
+#define ENCODE2_PIN GPIOA, GPIO_PIN_2
+#define WAVEIN1_PIN GPIOA, GPIO_PIN_3
+#define WAVEIN2_PIN GPIOA, GPIO_PIN_4
+#define LCD_SCK_PIN GPIOA, GPIO_PIN_5
+#define LCD_MISO_PIN GPIOA, GPIO_PIN_6
+#define LCD_MOSI_PIN GPIOA, GPIO_PIN_7
+#define LCD_DCRS_PIN GPIOC, GPIO_PIN_4
+#define LCD_RESET_PIN GPIOC, GPIO_PIN_5
+#define LCD_CS_PIN GPIOB, GPIO_PIN_0
+#define SD_SCK_PIN GPIOB, GPIO_PIN_13
+#define SD_MISO_PIN GPIOB, GPIO_PIN_14
+#define SD_MOSI_PIN GPIOB, GPIO_PIN_15
+#define SD_CS_PIN GPIOC, GPIO_PIN_6
+#define USART1_TX_PIN GPIOA, GPIO_PIN_9
+#define USART1_RX_PIN GPIOA, GPIO_PIN_10
+#define USB_DM_PIN GPIOA, GPIO_PIN_11
+#define USB_DP_PIN GPIOA, GPIO_PIN_12
+#define SWD_IO_PIN GPIOA, GPIO_PIN_13
+#define SWD_CLK_PIN GPIOA, GPIO_PIN_14
+#define TIM2_CH1_PIN GPIOA, GPIO_PIN_15
+#define ENCODE1_PIN GPIOC, GPIO_PIN_11
+#define TIM2_CH2_PIN GPIOB, GPIO_PIN_3
+#define SCALE_AXIS_PIN GPIOB, GPIO_PIN_4
+#define I2C1_SCL_PIN GPIOB, GPIO_PIN_6
+#define I2C1_SDA_PIN GPIOB, GPIO_PIN_7
 
+#define ADC_12BIT 4095.0f
+#define Sample_rate 50000 //Hz
+#define Sample_period 0.00002f //s (1/Sample_rate)
+#define ADC_BUFFER_SIZE 1024 //Size of ADC DMA buffer
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -92,6 +128,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
@@ -100,8 +137,11 @@ int main(void)
   MX_SPI2_Init();
   MX_TIM2_Init();
   MX_TIM5_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-
+  Hal_TIM_Base_Start(&htim3); //Start Timer 3 for ADC trigger
+  uint16_t adc_buffer[ADC_BUFFER_SIZE]; //ADC DMA buffer
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buffer, ADC_BUFFER_SIZE); //Start ADC with DMA
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -114,6 +154,56 @@ int main(void)
   }
   /* USER CODE END 3 */
 }
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    switch(GPIO_Pin)
+    {
+        case GPIO_PIN_14: // PC14 -- CHL_Select Button
+            // handle PC14 interrupt
+            break;
+        case GPIO_PIN_13: // PC13 -- Pause Button
+            // handle PC13 interrupt
+            break;
+        case GPIO_PIN_11: // PC11 -- Encoder 1 Button
+            // handle PC11 interrupt
+            break;
+        case GPIO_PIN_4:  // PB4 -- Scale Axis Button
+            // handle PB4 interrupt
+            break;
+        case GPIO_PIN_2:  // PA2 -- Encoder 2 Button
+            // handle PA2 interrupt
+            break;
+    }
+}
+
+void ADC_Scale(uint16_t adc_input, float *scaled_value){
+	//Scale ADC input based on schematic design
+	//ADC input range: 0-3.3V (0-4095), VRef = 3.3V
+	//Scope input range: +/-20v
+	//Attenuation factor: 50/51
+	//Bias: 3.3v/2 = 1.65v
+	//Calculation = (Input-Bias)/Attenuation
+	float Vin = (float)adc_input * (3.3/ADC_12BIT); //Convert ADC value to voltage
+	*scaled_value = (Vin - 1.65) * (51.0f/1.0f); //Scale to scope input voltage
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
   * @brief System Clock Configuration
@@ -132,16 +222,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLM = 24;
+  RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -151,12 +239,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
